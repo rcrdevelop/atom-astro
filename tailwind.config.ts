2214c5ba8068653da375efc087e1b9b958ca88d7
nathalydev <40666926+rcrdevelop@users.noreply.github.com>
2024-06-04 13:28:53 -0400

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@
 import themer from "@tailus/themer";
 
 module.exports = {
+    darkMode: 'selector',
     content: [
         './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
         "./node_modules/@tailus/themer-**/dist/**/*.{js,ts}"
